c50490461f1a7241e1edb69b35af9d483f3592f8
ALex Nikonov <alex@sshaman.ru>
2018-11-22 19:39:45 +0300

    fix: proper cancellation by context

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 	"flag"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/jinzhu/configor"
 	"github.com/sonm-io/core/accounts"
 	"github.com/sonm-io/core/cmd"
+	"github.com/sonm-io/core/insonmnia/logging"
+	"github.com/sonm-io/core/insonmnia/npp"
 	"github.com/sonm-io/core/toolz/metrics-collector/collector"
 	"github.com/sonm-io/core/toolz/metrics-collector/discovery"
 	"github.com/sonm-io/core/toolz/metrics-collector/exporter"
-	"github.com/sonm-io/core/insonmnia/logging"
-	"github.com/sonm-io/core/insonmnia/npp"
 	"github.com/sonm-io/core/util"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -81,8 +82,17 @@ func main() {
 	})
 
 	log.Info("starting metrics collector")
-	wg.Go(func() error {
-		for {
+
+	tk := util.NewImmediateTicker(time.Minute)
+	defer tk.Stop()
+
+x1:
+	for {
+		select {
+		case <-ctx.Done():
+			log.Warn("context done", zap.Error(ctx.Err()))
+			break x1
+		case <-tk.C:
 			workers, err := disco.List(ctx)
 			if err != nil {
 				log.Warn("failed to get workers from discovery", zap.Error(err))
@@ -91,35 +101,29 @@ func main() {
 
 			log.Info("workers collected", zap.Int("count", len(workers)))
 
-			for _, w := range workers {
-				worker := w
-				wg.Go(func() error {
-					s, err := collectro.Status(ctx, worker)
+			for _, worker := range workers {
+				go func(w common.Address) {
+					s, err := collectro.Status(ctx, w)
 					if err != nil {
-						log.Warn("failed to collect status", zap.Stringer("worker", worker), zap.Error(err))
-						return nil
+						// probably we should export smth like "failed: true" in such case
+						log.Warn("failed to collect status", zap.Stringer("worker", w), zap.Error(err))
+						return
 					}
 
-					err = exporto.Write(
-						worker,
-						map[string]float64{},
+					err = exporto.Write(w, map[string]float64{},
 						map[string]string{
 							"version": s.GetVersion(),
 							"geo":     s.GetGeo().GetCountry().GetIsoCode(),
 						})
 
 					if err != nil {
-						log.Warn("failed to write metrics", zap.Stringer("worker", worker), zap.Error(err))
+						log.Warn("failed to write metrics", zap.Stringer("worker", w), zap.Error(err))
 					}
-
-					return nil
-				})
+				}(worker)
 			}
-
-			log.Info("start waiting for 60s")
-			time.Sleep(60 * time.Second)
 		}
-	})
+	}
 
+	log.Debug("termination")
 	wg.Wait()
 }
